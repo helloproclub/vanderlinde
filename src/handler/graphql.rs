9a59328b63c12a86da548d5840641251e1fd0bd5
Ivan <iryansyach@outlook.co.id>
2020-09-23 15:59:36 +0700

    fixed dependencies

--- src/handler/graphql.rs
+++ src/handler/graphql.rs
@@ -1,28 +1,28 @@
-// use rocket::{response::content, State};
+use rocket::{response::content, State};
 
-// use crate::database::DbConn;
-// use crate::domain::user::User;
-// use crate::graphql::{GQLContext, Schema};
+use crate::database::DbConn;
+use crate::domain::user::User;
+use crate::graphql::{GQLContext, Schema};
 
-// #[rocket::post("/", data = "<request>")]
-// pub fn post_graphql_handler(
-//     database: DbConn,
-//     request: juniper_rocket::GraphQLRequest,
-//     auth: crate::lib::auth::Auth,
-//     schema: State<Schema>,
-// ) -> juniper_rocket::GraphQLResponse {
-//     // let user = User::get()
-//     let user = match auth.user_id {
-//         Some(id) => {
-//             let id = uuid::Uuid::parse_str(&id).unwrap();
-//             User::find_by_id(&database, id).unwrap()
-//         }
-//         None => None,
-//     };
-//     request.execute(&schema, &GQLContext { database, user })
-// }
+#[rocket::post("/", data = "<request>")]
+pub fn post_graphql_handler(
+    database: DbConn,
+    request: juniper_rocket::GraphQLRequest,
+    auth: crate::lib::auth::Auth,
+    schema: State<Schema>,
+) -> juniper_rocket::GraphQLResponse {
+    // let user = User::get()
+    let user = match auth.user_id {
+        Some(id) => {
+            let id = uuid::Uuid::parse_str(&id).unwrap();
+            User::find_by_id(&database, id).unwrap()
+        }
+        None => None,
+    };
+    request.execute(&schema, &GQLContext { database, user })
+}
 
-// #[rocket::get("/")]
-// pub fn graphiql() -> content::Html<String> {
-//     juniper_rocket::playground_source("/")
-// }
+#[rocket::get("/")]
+pub fn graphiql() -> content::Html<String> {
+    juniper_rocket::playground_source("/")
+}
--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -1,4 +1,4 @@
 pub mod auth;
-pub mod graphql;
+// pub mod graphql;
 pub mod ping;
 pub mod response;
