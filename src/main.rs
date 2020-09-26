e9dd95ad9f9316b406bd1e36185d1b87ac3460ff
Ganesha Danu <ganeshadanu1@gmail.com>
2020-09-26 20:12:49 +0700

    fix formatting

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
-#[macro_use] extern crate diesel;
-#[macro_use] extern crate serde_derive;
-#[macro_use] extern crate rocket_contrib;
-#[macro_use] extern crate serde_json;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_json;
 
 mod config;
 mod database;
